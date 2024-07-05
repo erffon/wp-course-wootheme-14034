c95440597c52370661b63d86777692b06f20b49f
erffon <erffon@gmail.com>
2024-07-05 19:55:52 +0330

    Feat: Add index newsletter - best seller and remained blocks

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 <?php get_template_part('partials/index/index-slider', 'index-slider') ?>
 <?php get_template_part('partials/index/index-ads', 'index-ads') ?>
 <?php get_template_part('partials/index/index-collection', 'index-collection') ?>
+<?php get_template_part('partials/index/index-middle-ads', 'index-middle-ads') ?>
+<?php get_template_part('partials/index/index-best-seller', 'index-best-seller') ?>
+<?php get_template_part('partials/index/index-newsletter', 'index-newsletter') ?>
 
 <?php get_footer('shop'); ?>
---
+++ partials/index/index-middle-ads.php
@@ -0,0 +1,16 @@
+<div class="e-midads-wrappper">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="e-topads-inner e-cads-inner">
+                    <a href="product_category_sidebar.html"><img src="<?php echo THEME_URI . '/assets/images/index1/mid_ads1.jpg' ?>" alt="image-1" class="img-fluid"></a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="e-topads-inner e-cads-inner">
+                    <a href="product_category_sidebar.html"><img src="<?php echo THEME_URI . '/assets/images/index1/mid_ads2.jpg' ?>" alt="image-2" class="img-fluid"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

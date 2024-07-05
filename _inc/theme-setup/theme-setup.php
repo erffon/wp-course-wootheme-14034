88ead29a0c194727df4da5e1490a53bfd3d373ec
erffon <erffon@gmail.com>
2024-07-05 22:59:20 +0330

    Fix: Remove admin bar

--- _inc/theme-setup/theme-setup.php
+++ _inc/theme-setup/theme-setup.php
@@ -4,5 +4,6 @@ function wct_theme_setup()
 {
     add_theme_support('woocommerce');
     add_theme_support('post-thumbnails');
+    add_filter('show_admin_bar', '__return_false');
 }
 add_action('after_setup_theme', 'wct_theme_setup');
